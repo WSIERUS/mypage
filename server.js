59919b706751e107a1c4390338dfdbb5f3495e59
Wojciech <wojt.sier@o2.pl>
2022-04-21 21:33:34 +0200

    heroku 9

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('views engine', 'html');
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: path.join(__dirname + '/client/public')
+  });
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
