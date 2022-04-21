0d0374be1c810338f8e5b30541aabd4ce6ea77bc
Wojciech <wojt.sier@o2.pl>
2022-04-21 20:33:24 +0200

    heroku 5

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Route działa')
+  res.render('Route działa')
 })
 
 outer.get('*', (req, res) => {
