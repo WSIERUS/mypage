2f889206a1b4620a18a50ec39797005a313d597c
Wojciech <wojt.sier@o2.pl>
2022-04-21 20:14:17 +0200

    heroku 2

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-
-router.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-router.get('*', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
