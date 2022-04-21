ce3f67dd98be8f4eb24d863844ed35ca81d06e30
Wojciech <wojt.sier@o2.pl>
2022-04-21 20:30:05 +0200

    heroku 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import React, {useEffect} from 'react'
 
 function App() {
--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('*', (req, res) => {
+router.get('/', (req, res) => {
+  res.send('Route działa')
+})
+
+outer.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-
-router.get('/', (req, res) => {
-  res.render('index')
-})
 
 module.exports = router
