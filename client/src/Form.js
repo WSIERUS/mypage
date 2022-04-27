4309ede0272790a633ad6f476cb2ee870e53f156
Wojciech <wojt.sier@o2.pl>
2022-04-27 22:55:39 +0200

    routing test 1

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,14 +1,6 @@
 import React from 'react'
 
 function Form() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
       Tutaj będzie formularz
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Form from './Form'
@@ -9,20 +9,8 @@ function renderRoot() {
   root.render(
     <React.StrictMode>
       <App />
+      {fetch('/form', {}).then(
+        () => <Form/>
+      )}
     </React.StrictMode>
-  );
-}
-
-function renderFormRoot() {
-  const formRoot = ReactDOM.createRoot(document.getElementById('form-root'));
-  formRoot.render(
-    <React.StrictMode>
-      <Form />
-    </React.StrictMode>
-  );
-}
-
-renderRoot()
-renderFormRoot()
-
-
+  )}
