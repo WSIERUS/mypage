364afd1c665f3fdd3d5229fa283e493eadeb225e
Wojciech <wojt.sier@o2.pl>
2022-05-04 20:25:38 +0200

    routes commit 3

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './index.css';
+
 import App from './App';
+import Form from './Form'
 
 function renderRoot() {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='form' element={<Form/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
