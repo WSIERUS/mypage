f194b178639b4ebd613e13d2a87469bcdf47b5ed
Wojciech <wojt.sier@o2.pl>
2022-05-04 19:21:53 +0200

    router commit 1

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Form from './Form'
 
 function renderRoot() {
   const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,4 +12,3 @@ function renderRoot() {
   )
 }
 
-// Tutaj będzie obsługa react router - walić
