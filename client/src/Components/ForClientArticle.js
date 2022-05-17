7a08d9ddccd16dd9e6117d58b30747745c133292
Wojciech <wojt.sier@o2.pl>
2022-05-17 13:30:44 +0200

    basic context for client

--- client/src/Components/ForClientArticle.js
+++ client/src/Components/ForClientArticle.js
@@ -4,14 +4,24 @@ import './styles/ForClientArticle.css'
 
 const ForClientArticle = ({article}) => {
 
-  const {id, src, description} = article
+  const {id, src, title, descriptions} = article
 
-  console.log(id,src,description)
+  // console.log(id, src, title, descriptions)
+
+  let indexArticleDescription = 0
 
   return(
     <div className={id%2 ? "forclient-article-right" : "forclient-article-left"}>
-    <article className="forclient-article-description">{description}</article>
-    <img src={src} alt="" className="forclient-article-img"/>
+      <div className="forclient-article">
+        {title ? <article className="forclient-article-title">{title}</article> : null}
+        {descriptions.map(description => {
+          indexArticleDescription++
+          return <article className="forclient-article-description" key={indexArticleDescription}>
+            {description}
+          </article>
+        })}
+      </div>
+      <img src={src} alt="" className="forclient-article-img"/>
     </div>
   )
 }
--- client/src/Context.js
+++ client/src/Context.js
@@ -1,16 +1,14 @@
 import { createContext } from "react";
 
-import Test from './Icons/Mouse.png'
-
 const store = {
   contact : {
     phone:'+48 668 107 846',
     email:'wojciechsieradzki35@gmail.com'
   },
   headNaviButtons : [
-    {id:0, name:'O mnie', scrollToPlace: 'aboutme'},
-    {id:1, name:'Dla klientów', scrollToPlace: 'forclient'},
-    {id:2, name:'Portfolio', scrollToPlace: 'portfolio'},
+    {id:0, name:'START', scrollToPlace: 'aboutme'},
+    {id:1, name:'KLIENT', scrollToPlace: 'forclient'},
+    {id:2, name:'O NAS', scrollToPlace: 'portfolio'},
   ],
   aboutMeDescriptions : [
     {id:0, description: 'Profesjonalna wycena oraz analiza potrzeb klienta'},
@@ -18,8 +16,39 @@ const store = {
     {id:2, description: 'Solidna jakość obsługi klienta'},
   ],
   forClientArticles : [
-    {id:0, src: Test, description:'Opis testowy post 1'},
-    {id:1, src: Test, description:'Opis testowy post 2'},
+    {
+      id:0, src: null, 
+      title:'Dostosowanie na urządzenia mobilne', 
+      descriptions:[
+        'Nawet 80% użytkowników odwiedzających strony internetowe korzysta z smartfonu.',
+        'Dlatego bardzo ważne jest stworzenie strony responsywnej dla każdej rozdzielczości oraz orientacji urządzenia.'
+      ],
+    },
+    {
+      id:1, src: null, 
+      title:'Profesjonalna analiza potrzeb twojego klienta', 
+      descriptions:[
+        'Dostosowanie strony w odpowiedni sposób nie tylko pozwoli zwiększyć portfel klientów ale również podnieść poziom zadowolenia z oferowanych usług.',
+        'Kwestia zaprojektowania strony jest istotnym elementem całego procesu.'
+      ],
+    },
+    {
+      id:2, src: null, 
+      title:'Odpowiednia wycena', 
+      descriptions:[
+        'Do każdego projektu podchodzimy indywidualnie i szukamy odpowiednich rozwiązań',
+        'Nie działamy szablonowo.',
+        'Wychodzimy z założenia, że każdy klient ma inne oczekiwania względem oferowanych usług'
+      ],
+    },
+    {
+      id:3, src: null, 
+      title:'Obsługa po zakończonym projekcie', 
+      descriptions:[
+        'Możesz liczyć na naszą pomoc nawet po zakończonym projekcie.',
+        'Dajemy gwarancję zadowolenia z oddanego produktu.',
+      ],
+    },
   ]
 }
 
