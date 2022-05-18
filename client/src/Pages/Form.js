ce5df76697d5a62005e5b7a9da60f88ec41bffe3
Wojciech <wojt.sier@o2.pl>
2022-05-18 20:49:06 +0200

    Create Form Page

--- client/src/Pages/Form.js
+++ client/src/Pages/Form.js
@@ -1,11 +1,42 @@
 import React from 'react'
 
+import './styles/Form.css'
+
 function Form() {
   return (
-    <div className="App">
-      Tutaj będzie formularz
-    </div>
-  ); // Formularz wysyłany do mongoDB i odbierany w innej aplikacji w postaci To Do List
+    <form className="form">
+
+      <div>Zostaw do siebie kontakt. Oddzwonimy!</div>
+
+      <div>
+        <div>Imię</div>
+        <input type="text" />
+      </div>
+
+      <div>
+        <div>NIP</div>
+        <input type="number" />
+      </div>
+
+      <div>
+        <div>Branża</div>
+        <input type="text" />
+      </div>
+
+      <div>
+        <div>Numer telefonu</div>
+        <input type='number'/>
+      </div>
+
+      <div>
+        <div>Adres E-mail</div>
+        <input type="email" />
+      </div>
+
+      <input type="submit" name='submit' value='Wyślij!'/>
+
+    </form>
+  );
 }
 
 export default Form;
