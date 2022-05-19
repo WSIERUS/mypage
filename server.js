5141663276aa369f25fa690356977c92d6520044
Wojciech <wojt.sier@o2.pl>
2022-05-19 20:34:01 +0200

    routes problem fix 1

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const bodyParser = require('body-parser')
 
 const PORT = process.env.PORT || 3001;
 
+const mainRouter = require('./routes/main')
+const formRouter = require('./routes/form')
+
 const app = express();
 
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const mainRouter = require('./routes/main')
-const formRouter = require('./routes/form')
-
 app.engine('html', require('ejs').renderFile);
 app.set('views engine', 'html');
 
+app.use(express.static(__dirname + '/client/build'));
+
 app.use('/', mainRouter)
 app.use('form', formRouter)
-
-app.use(express.static(__dirname + '/client/build'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
